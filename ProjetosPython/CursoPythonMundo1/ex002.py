8a05141bb155c006165f45abf3fdde9855fabfbd
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-04 12:26:18 -0300

    #montando um código que receba seu nome e mostre a mensagem de boas vinda com o nome! Com Curso em vídeo

---
+++ ProjetosPython/CursoPythonMundo1/ex002.py
@@ -0,0 +1,5 @@
+print('Desafio 002')
+#monte um código que receba seu nome e mostre a mensagem de boas vinda com o nome!
+
+nome = input('Digite seu nome:')
+print('Seja bem vindo \033[4;31m {}'.format(nome))
