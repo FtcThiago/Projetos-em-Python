da01e7ce3ccef54b9c6a264caef1693c58efe7ec
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-20 17:36:40 -0300

    Create ex010.py

---
+++ ProjetosPython/CursoPythonMundo1/ex010.py
@@ -0,0 +1,9 @@
+print('DESAFIO 010')
+
+print('_____Convertor de Reais para Dólares_____')#Um programa para coverter Reais em Dólares
+
+reais = float(input('Iforme o valor total de reais:R$'))#Com o dinheiro é necessário usar o "aritimético" float
+
+print("Convertendo {} em Dólares, você obtém.....\nCalculando....\nvalor do dólar:3,27...\nCalculando... "
+      "\nVocê possui {:.2f} Dólares.'".format(reais, reais/3.27))
+
