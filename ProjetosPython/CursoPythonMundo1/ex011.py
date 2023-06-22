ae43bef457fb9e961fb46f44c3bba2539a9c52a8
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-22 09:10:51 -0300

    Create ex011.py

---
+++ ProjetosPython/CursoPythonMundo1/ex011.py
@@ -0,0 +1,11 @@
+print('DESAFIO 011')
+
+print('Calculando a Área da parede e quantas tintas serão necessárias!')#Calculando a Área da parede e quantas tintas serão necessárias!
+largura = float(input('Informe a Largura da Parede:'))
+altura = float(input('Informe a Altura da Parede:'))
+
+print('Arca:Ativando programa....\n')
+print('\nCalculando Área....\nLatas de Tintas a serem usadas.....\nCalculando....')
+print('Área da parede:{}'
+      '\nLitro de tintas necessárias:{}'.format(largura*altura, largura*altura/2))
+print('\nArca:Programa,finalizado. Tenha um bom dia.')
