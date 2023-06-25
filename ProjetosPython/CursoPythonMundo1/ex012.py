9dc6c39c5720939c980eb2b5c453fc26ba764fce
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-25 10:23:44 -0300

    Create ex012.py

---
+++ ProjetosPython/CursoPythonMundo1/ex012.py
@@ -0,0 +1,15 @@
+print('DESAFIO 012')
+
+produto = float(input('Digite o preço do produto:R$'))
+
+print('-----------------------------------------------------------------'
+      '\nArca:Aqui temos um programa bem útil para mercados! vamos testa-lo!'
+      '\n-----------------------------------------------------------------')
+print('Preço do produto:{}'
+      '\n   Calculando....\n   liberando desconto(5%)....\n   tem observado os passáros?...\n   calculando....'
+      '\nPreço do produto com desconto:R${:.2f}'.format(produto, produto-(produto*(5/100))))
+print('-----------------------------------------------------------------'
+      '\nArca:Programa finalizado.....uma observção:tenho observado os passáros, e percebo que são divertidos,belos '
+      '\ne egoístas!...(não dividem o alimento!mesmo após terem comida tanto e estarem satisfeitos)mas ainda belos.'
+      '\n-----------------------------------------------------------------')
+
