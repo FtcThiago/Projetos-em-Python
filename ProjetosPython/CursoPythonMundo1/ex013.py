ae8231fa4cc4bfeeb5d1592c11a14f4fb895447f
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-26 09:04:51 -0300

    Create ex013.py

---
+++ ProjetosPython/CursoPythonMundo1/ex013.py
@@ -0,0 +1,13 @@
+print('DESAFIO 013')
+
+print('-----------------------------------------------------------------'
+      '\nArca:iremos calcular o novo sálario,acrescentando seu aumento!'
+      '\n-----------------------------------------------------------------')
+salario = float(input('Arca:Digite o salário:R$'))
+print('Arca:O Sálario atual é:R${:.2f}'
+      '\n   Acrescentando o aumento de 15% temos:....\n   calculando....\n   você gosta do seu trabalho?....\n   acrescentando....'
+      '\nArca: O novo salário é:R${:.2F}'.format(salario, salario+(salario*(15/100))))
+print('-----------------------------------------------------------------'
+      '\nArca:Finalizando programa.....Levando em conta que passas muita da sua vida no trabalho,'
+      '\ncertamente deves ter escolhido algo que lhe alegre e lhe deixe realizado,certo?'
+      '\n-----------------------------------------------------------------')
