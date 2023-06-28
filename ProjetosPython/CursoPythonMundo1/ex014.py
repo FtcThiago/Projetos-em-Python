0126787f1c18c7d6c96dc45b4f908ec3a4416584
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-28 14:03:42 -0300

    Create ex014.py

---
+++ ProjetosPython/CursoPythonMundo1/ex014.py
@@ -0,0 +1,16 @@
+print('DESAFIO 014')
+
+#variaveis
+celsius = float(input('Arca:Informe a temperatura em C°:'))
+fahrenheit = (celsius * 9)/5 + 32
+
+
+print('-----------------------------------------------------------------'
+      '\nArca: transformando a temperatura em grau celcius para fahrenheit: '
+      '\nCalculando....\nanalisando temperatura em fahrenheit....'
+      '\n-----------------------------------------------------------------')
+
+
+print(f'A temperatura de {celsius} grau celsius para fahrenheit é :{fahrenheit}°F')
+print('Logo a achamos {1}°F com {0}°C'.format(celsius, fahrenheit))
+
