1dec97a8db09401bc1ca110778dc74f6bbce3760
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-06-30 09:03:59 -0300

    Create ex016.py

---
+++ ProjetosPython/CursoPythonMundo1/ex016.py
@@ -0,0 +1,14 @@
+#DESAFIO 016
+
+#Importações
+#Arca:Importando bliblioteca math( que contem mais facilidades e comandos a mais de matematicas)
+from math import trunc
+
+
+# VÁRIAVEIS
+
+num = float(input('Arca:Informe um numero real:'))
+
+print(f'Arca: -O número digitado: {num} \n -Sua parte inteira é:{trunc(num)}') #print(f'Arca: -O número digitado: {num} \n -Sua parte inteira é:{int(num)}')
+
+print('Arca:Programa finalizado....')
