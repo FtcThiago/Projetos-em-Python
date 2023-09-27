ad2e80feab3345021e35e0887d86e9e3a5bbd754
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-09-27 08:03:48 -0300

    Create ex030.py

---
+++ ProjetosPython/CursoPythonMundo1/ex030.py
@@ -0,0 +1,16 @@
+#DESAFIO 030
+
+#030 Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR
+
+
+num = int(input('Arca:Informe um número inteiro qualquer:'))
+
+
+if num % 2 ==0:
+    print('Arca: O número escolhido é PAR')
+else:
+    print('Arca:O número escolhido é ÍMPAR')
+
+print('\n----------'
+      '\nArca:Programa finalizado')
+
