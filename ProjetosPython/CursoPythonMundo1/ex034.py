a5fc964b61a86c27a0c3fc9728e32ccc138376b3
Thiago <133411658+FtcThiago@users.noreply.github.com>
2023-09-29 08:29:13 -0300

    Create ex034.py

---
+++ ProjetosPython/CursoPythonMundo1/ex034.py
@@ -0,0 +1,18 @@
+#DESAFIO 034
+
+#034 Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
+#Para salários superiores a R$1.250,00, calcule um aumento de 10%
+#Para os inferiores ou iguais, o aumento é de 15%
+
+
+
+salario = float(input('Arca:Informe o salário:'))
+
+if salario > 1250:
+    print(f'Arca:Você recebeu um aumento de 10%!'
+          f'\nSeu salário atual R${salario}'
+          f'\nSeu novo salário:R${salario+(salario*(10/100))}')
+if salario <= 1250:
+    print(f'Arca:Você recebeu um aumento de 15%!'
+          f'\nSeu salário atual: R${salario}'
+          f'\nSeu novo salário: R${salario+(salario*(15/100))}')
